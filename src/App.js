ca876659b0798139658ca9bd1da0e4336d9bf675
sarnakhmel <OlSa@MacBook-Pro-username.local>
2021-12-09 17:01:18 +0200

    hooks useState

--- src/App.js
+++ src/App.js
@@ -8,12 +8,18 @@ function computeInitialCalculation(){
 }
 
 
+
 function App() {
 // const [counter, setCounter] = useState(computeInitialCalculation());
 // const [counter, setCounter] = useState(computeInitialCalculation);
 const [counter, setCounter] = useState(() => {
    return computeInitialCalculation();
 });
+
+const [state, setState] = useState({
+  title: "Counter",
+  date: Date.now()
+})
 
 function increment() {
   // setCounter(counter + 1);
@@ -26,11 +32,24 @@ function decrement() {
   setCounter(counter - 1);
 }
 
+function updateTitles() {
+  setState(prev => {
+    return {
+      ...prev,
+      title: "new state"
+    }
+  })
+}
+
   return (
     <div className="App">
         <h1>Counter: {counter}</h1>
         <button onClick={increment} className="btn btn-success" >increment</button>
         <button onClick={decrement} className="btn btn-danger" >decrement</button>
+
+        <pre>{JSON.stringify(state, null, 2)}</pre>
+        <button onClick={updateTitles} className="btn btn-default"> increment</button>
+
     </div>
   );
 }
